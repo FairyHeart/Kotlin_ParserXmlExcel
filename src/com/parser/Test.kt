332fed7ec630bc63df990a88d91179839bd67058
guazi <guazi@2dfire.com>
2019-12-31 17:56:46 +0800

    add: excel转string.xml文件

--- src/com/parser/Test.kt
+++ src/com/parser/Test.kt
@@ -1,12 +1,19 @@
 package com.parser
 
 /**
- *
+ * 测试类
  *
  * @author: Guazi.
  * @date  : 2019-12-30.
  */
+
 fun main() {
+
+    val src = "d://parserExcel"
+
+//    val fileName = "kds.xls"
+    val fileName = "waiting.xls"
+
     val excelFile = ReadExcelFile()
-    excelFile.startParserFile()
+    excelFile.startParserFile(src = src, fileName = fileName)
 }
--- src/com/parser/utils/IOUtils.kt
+++ src/com/parser/utils/IOUtils.kt
@@ -1,20 +1,24 @@
 package com.parser.utils
 
+import jxl.Workbook
 import java.io.Closeable
 import java.io.IOException
 
 /**
- *
+ * 工具类
  *
  * @author: Guazi.
  * @date  : 2019-12-30.
  */
 object IOUtils {
 
-    fun close(input: Closeable?) {
+    fun close(input: Closeable?, wb: Workbook? = null) {
         try {
+            wb?.close()
             input?.close()
         } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
+
 }
